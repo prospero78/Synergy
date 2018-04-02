f648d02c710a85806477a42e25ae72a1a6cb80b9
prospero78su <prospero.78.su@gmail.com>
2018-04-02 20:40:51 +0200

    23 Добавлена обработка нажатия клавиш.

--- tools/terminal/terminal.pas
+++ tools/terminal/terminal.pas
@@ -4,11 +4,25 @@
 
 uses GraphABC, Font8x8;
 
+/// Первоначальная настройка экрана 
 procedure Начать;
 begin
-Window.Title:='Test terminal';
-end; 
+   Window.Title := 'Test terminal';
+   Window.Height := 480;
+   Window.Width := 640;
+   Window.Clear(clBlack);
+   // Буквы мелкие,подправим масштаб
+   Coordinate.SetScale(2);
+   //Установим цвет пера
+   Pen.Color := clWhite;
+end;
+
+/// Процедура обработки нажатий на клавиши
+procedure Клава_Клик(лит: char);
+begin
+   Writeln('Клик! ' + лит);
+end;
 
 begin
-
-end.
+   OnKeyPress := Клава_Клик;
+end. 
