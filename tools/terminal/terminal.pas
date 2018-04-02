eff6ae243f83db5965a2a36bac3be1051db920c2
prospero78su <prospero.78.su@gmail.com>
2018-04-02 19:42:09 +0200

    20 Первая попытка эмулятора терминала.

---
+++ tools/terminal/terminal.pas
@@ -0,0 +1,14 @@
+﻿library terminal;
+{ Библиотека представляет из себя эмулятор терминала.
+Умеет воспринимать нажатые клавиши и отображать их на экране. }
+
+uses GraphABC;
+
+procedure Начать;
+begin
+Window.Title:='Test terminal';
+end; 
+
+begin
+
+end.
