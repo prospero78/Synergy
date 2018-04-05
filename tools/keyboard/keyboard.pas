ae98deda44701f0f3714dfba82d9352c5090caec
prospero78su <prospero.78.su@gmail.com>
2018-04-05 11:26:13 +0200

    36 Добавлен эмулятор клавиатуры.

---
+++ tools/keyboard/keyboard.pas
@@ -0,0 +1,21 @@
+﻿library keyboard;
+{ Библиотека отслеживает нажатие всех возможных клавиш, конвертирует их
+в строки, отправляет в место, куда назначено при запуске клавиатуры }
+
+uses GraphABC;
+
+type
+   тКлава = class
+      передать: procedure(кнп: string);
+      procedure Кнп_Нажата(лит: char);
+      begin
+         var стр := лит;
+          self.передать(стр);
+      end;
+      
+      constructor Create;
+      begin
+         OnKeyPress := self.Кнп_Нажата;
+      end;
+   end;
+end.
